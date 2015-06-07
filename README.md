f36ae8fc003c688b896ef80b05c08965d7fb2b0b
Noel Maddy <zhtwnpanta@gmail.com>
2015-06-07 08:28:03 -0600

    Initial commit - minted with Dist::Milla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+DBIx::Class::InflateColumn::DateTime::WithTimeZone - Blah blah blah
+
+# SYNOPSIS
+
+    use DBIx::Class::InflateColumn::DateTime::WithTimeZone;
+
+# DESCRIPTION
+
+DBIx::Class::InflateColumn::DateTime::WithTimeZone is
+
+# AUTHOR
+
+Noel Maddy <zhtwnpanta@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2015- Noel Maddy
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/DBIx/Class/InflateColumn/DateTime/WithTimeZone.pm
@@ -0,0 +1,39 @@
+package DBIx::Class::InflateColumn::DateTime::WithTimeZone;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Class::InflateColumn::DateTime::WithTimeZone - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use DBIx::Class::InflateColumn::DateTime::WithTimeZone;
+
+=head1 DESCRIPTION
+
+DBIx::Class::InflateColumn::DateTime::WithTimeZone is
+
+=head1 AUTHOR
+
+Noel Maddy E<lt>zhtwnpanta@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Noel Maddy
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
